7c9cf0f64b69d33d592e59453a0790bd9c965683
Nodirbek <berdimurodovnodir89@gmail.com>
2025-12-27 14:34:21 +0500

    bajarildi

--- task06.py
+++ task06.py
@@ -1,1 +1,11 @@
 
+text = 'olol radar makka non kiyik'
+result = text.split()
+palindromlar = []
+total = ",".join(result)
+for son in text:
+    if son == son[::-1]:
+     palindromlar.append(son)
+     
+
+print(palindromlar)
